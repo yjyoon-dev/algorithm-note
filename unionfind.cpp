982cba6393720f7630ec711a9e7c61dc99c134ef
unknown <yjyoon.dev@gmail.com>
2021-01-05 21:46:45 +0900

    edit code

--- unionfind.cpp
+++ unionfind.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 struct Unionfind{
     vector<int> parent, rank;
-    void makeSet(int n){
-        parent=vector<int>(n);
-        for(int i=0;i<n;i++)
+    Unionfind(int n){
+        parent=vector<int>(n+1);
+        for(int i=1;i<=n;i++)
             parent[i]=i;
-        rank=vector<int>(n,1);
+        rank=vector<int>(n+1,1);
     }
     int ufFind(int n){
         if(parent[n]==n) return n;
-        return parent[n]=ufFind(parent[n]); 
+        return parent[n]=ufFind(parent[n]);  // 경로 압축 기법
     }
     void ufUnion(int a, int b){
         a = ufFind(a), b = ufFind(b);
